0ef184fd283199b1edba9f89f3c5c60d0c67e112
Tianhong Zhang <zhang.tianhong@gmail.com>
2013-04-18 16:56:46 -0400

    update activity model
    activity model result should include off target search result, if exists

--- script/ASOActivityModel.sh
+++ script/ASOActivityModel.sh
@@ -1,7 +1,14 @@
 runAntisenseDesign.py EnumerationResult.csv
 antisense_predictor.py AOBase AOBase_542seq_cleaned_modelBuilding_Jan2009_15_21_noOutliers.csv c_a_thermo predict EnumerationResult_clean.csv < input_15_21_100_1000_12.txt
 rm EnumerationResult_clean.csv
-mergeASO.py EnumerationResult.csv OuTpUt_ReSuLtS.csv
+
+if [ -f ASOOffTargetSearchResult.csv ]; then
+	mergeASO.py ASOOffTargetSearchResult.csv OuTpUt_ReSuLtS.csv
+else
+	mergeASO.py EnumerationResult.csv OuTpUt_ReSuLtS.csv
+fi
+
+rm antisense_descriptors.pyc antisense_load_dataset.pyc antisense_stats.pyc
 
 
 
--- script/siRNAActivityModel.sh
+++ script/siRNAActivityModel.sh
@@ -3,10 +3,14 @@ get_overhangs.py target.txt txt oligo2predict_noOverhangs.csv
 lowercase.py oligo2predict.csv
 siRNA_predictor.py novartis siRNA_2431seq_modelBuilding.csv p_c_a_thermo predict oligo2predict_clean.csv
 
-cp EnumerationResult.csv outputSummaryBeforeMerge.csv
+if [ -f siRNAOffTargetSearchResult.csv ]; then
+    cp siRNAOffTargetSearchResult.csv outputSummaryBeforeMerge.csv
+else
+    cp EnumerationResult.csv outputSummaryBeforeMerge.csv
+fi
  
 cat OuTpUt_ReSuLtS.csv|cut -f 1-3,5-9,18,22-24 -d ',' |sed 's/siRNA_id,/name,/'|sed 's/G:C_content/GC_content/'|tr ',' '\t' >svm_out.txt
 cat outputSummaryBeforeMerge.csv|tr ',' '\t' |mergeEx.pl 1 1 svm_out.txt keepAll 1|tr '\t' ','>siRNAActivityModelResult.csv
 
-
+rm siRNA_descriptors.pyc load_dataset.pyc siRNA_stats.pyc
 
